ceb7b33b872b78129db29d97c676201f8da6a369
VU DUC TIEP <vuductiep@live.com>
2023-07-11 00:35:45 +0900

    project setting

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className='p-4'>
+      <Button size={'default'} variant={'outline'}>Click Me</Button>
+    </div>
+  )
+}
