f3d006a0f0ebf708d37b3d58d851a411b1f056e9
Vu Duc Tiep <vuductiep@live.com>
2023-07-15 20:43:55 +0900

    add prisma planetscale

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,23 @@
 "use client";
 
+import { StoreModal } from '@/components/modals/store-modal';
 import { Modal } from '@/components/ui/modal'
+import { useStoreModal } from '@/hooks/use-store-modal';
+import { useEffect } from 'react';
 
 const SetupPage = () => {
+  const onOpen = useStoreModal((state) => state.onOpen)
+  const isOpen = useStoreModal((state) => state.isOpen)
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen()
+    }
+  }, [isOpen, onOpen])
+
   return (
     <div className='p-4'>
-      <Modal title='Test' description='Test Desc' isOpen onClose={() => {}}>
-      </Modal>
+      Root Page
     </div>
   )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
+import { ModalProvider } from "@/providers/modal-provider";
+import prismadb from "@/lib/prismadb";
+import { ToasterProvider } from "@/providers/toast-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +21,11 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <ToasterProvider />
+          <ModalProvider />
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
