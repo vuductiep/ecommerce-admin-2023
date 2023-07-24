4e21d2561b409a7beacbe6967ca7e9c6049ceb51
Vu Duc Tiep <vuductiep@gaion.kr>
2023-07-25 00:09:00 +0900

    add color

--- app/(dashboard)/[storeId]/(routes)/colors/[colorId]/components/color-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/colors/[colorId]/components/color-form.tsx
@@ -8,7 +8,7 @@ import ImageUpload from "@/components/ui/image-upload"
 import { Input } from "@/components/ui/input"
 import { Separator } from "@/components/ui/separator"
 import { zodResolver } from "@hookform/resolvers/zod"
-import { Size } from "@prisma/client"
+import { Color } from "@prisma/client"
 import axios from "axios"
 import { Trash } from "lucide-react"
 import { useParams, useRouter } from "next/navigation"
@@ -19,27 +19,29 @@ import { z } from "zod"
 
 const formSchema = z.object({
   name: z.string().min(1),
-  value: z.string().min(1)
+  value: z.string().min(4).regex(/^#/, {
+    message: 'String must be a valid hex code'
+  })
 })
 
-interface SizeFormProps {
-  initialData: Size | null
+interface ColorFormProps {
+  initialData: Color | null
 }
 
-type SizeFormValues = z.infer<typeof formSchema>
+type ColorFormValues = z.infer<typeof formSchema>
 
-export const SizeForm: React.FC<SizeFormProps> = ({
+export const ColorForm: React.FC<ColorFormProps> = ({
   initialData
 }) => {
   const [open, setOpen] = useState(false)
   const [loading, setLoading] = useState(false)
 
-  const title = initialData ? "Edit a size" : "Create size"
-  const description = initialData ? "Edit a size" : "Add a new size"
-  const toastMessage = initialData ? "Size updated." : "Size created."
+  const title = initialData ? "Edit a color" : "Create color"
+  const description = initialData ? "Edit a color" : "Add a new color"
+  const toastMessage = initialData ? "Color updated." : "Color created."
   const action = initialData ? "Save changes" : "Create"
 
-  const form = useForm<SizeFormValues>({
+  const form = useForm<ColorFormValues>({
     resolver: zodResolver(formSchema),
     defaultValues: initialData || {
       name: '',
@@ -49,16 +51,16 @@ export const SizeForm: React.FC<SizeFormProps> = ({
   const params = useParams()
   const router = useRouter()
 
-  const onSubmit = async (data: SizeFormValues) => {
+  const onSubmit = async (data: ColorFormValues) => {
     try {
       setLoading(true)
       if (initialData){
-        await axios.patch(`/api/${params.storeId}/sizes/${params.sizeId}`, data)
+        await axios.patch(`/api/${params.storeId}/colors/${params.colorId}`, data)
       } else {
-        await axios.post(`/api/${params.storeId}/sizes`, data)
+        await axios.post(`/api/${params.storeId}/colors`, data)
       }
       router.refresh()
-      router.push(`/${params.storeId}/sizes`)
+      router.push(`/${params.storeId}/colors`)
       toast.success(toastMessage)
     } catch (error) {
       toast.error("Something went wrong")
@@ -70,12 +72,12 @@ export const SizeForm: React.FC<SizeFormProps> = ({
   const onDelete = async () => {
     try {
       setLoading(true)
-      await axios.delete(`/api/${params.storeId}/sizes/${params.sizeId}`)
+      await axios.delete(`/api/${params.storeId}/colors/${params.colorId}`)
       router.refresh()
-      router.push(`/${params.storeId}/sizes`)
-      toast.success("Size deleted.")
+      router.push(`/${params.storeId}/colors`)
+      toast.success("Color deleted.")
     } catch (error) {
-      toast.error("Make sure you removed all products using this size first.")
+      toast.error("Make sure you removed all products using this color first.")
     } finally {
       setLoading(false)
       setOpen(false)
@@ -118,7 +120,7 @@ export const SizeForm: React.FC<SizeFormProps> = ({
                 <FormItem>
                   <FormLabel>Name</FormLabel>
                   <FormControl>
-                    <Input disabled={loading} placeholder="Size name" {...field} />
+                    <Input disabled={loading} placeholder="Color name" {...field} />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
@@ -131,7 +133,13 @@ export const SizeForm: React.FC<SizeFormProps> = ({
                 <FormItem>
                   <FormLabel>Value</FormLabel>
                   <FormControl>
-                    <Input disabled={loading} placeholder="Size value" {...field} />
+                    <div className="flex items-center gap-x-4">
+                      <Input disabled={loading} placeholder="Color value" {...field} />
+                      <div 
+                        className="border p-4 rounded-full" 
+                        style={{ backroundColor: field.value}}
+                      />
+                    </div>
                   </FormControl>
                   <FormMessage />
                 </FormItem>
--- app/(dashboard)/[storeId]/(routes)/colors/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/colors/components/client.tsx
@@ -6,15 +6,15 @@ import { Billboard } from "@prisma/client"
 import { Separator } from "@radix-ui/react-separator"
 import { Plus } from "lucide-react"
 import { useParams, useRouter } from "next/navigation"
-import { SizeColumn, columns } from "./columns"
+import { ColorColumn, columns } from "./columns"
 import { DataTable } from "@/components/ui/data-table"
 import { ApiList } from "@/components/ui/api-list"
 
-interface SizeClientProps {
-  data: SizeColumn[]
+interface ColorClientProps {
+  data: ColorColumn[]
 }
 
-export const SizeClient: React.FC<SizeClientProps> = ({
+export const ColorClient: React.FC<ColorClientProps> = ({
   data
 }) => {
   const router = useRouter()
@@ -24,19 +24,19 @@ export const SizeClient: React.FC<SizeClientProps> = ({
     <>
       <div className="flex items-center justify-between">
         <Heading 
-          title={`Sizes (${data.length})`}
-          description="Manage sizes for your store"
+          title={`Colors (${data.length})`}
+          description="Manage colors for your store"
         />
-        <Button onClick={() => router.push(`/${params.storeId}/sizes/new`)}>
+        <Button onClick={() => router.push(`/${params.storeId}/colors/new`)}>
           <Plus className="h-4 w-4 mr-2" />
           Add New
         </Button>
       </div>
       <Separator />
       <DataTable columns={columns} data={data} searchKey="name"/>
-      <Heading title="API" description="API calls for Sizes"/>
+      <Heading title="API" description="API calls for Colors"/>
       <Separator />
-      <ApiList entityIdName="sizeId" entityName="sizes"/>
+      <ApiList entityIdName="colorId" entityName="colors"/>
     </>
   )
 }
